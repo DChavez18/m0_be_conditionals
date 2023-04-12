e6fee8a48555df3a24cb84303691b6a71a264ac8
Derek Chavez <frrenose17@yahoo.com>
2023-04-12 11:59:25 -0600

    add answers to comparisons

--- comparisons.rb
+++ comparisons.rb
@@ -25,21 +25,27 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >=21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -54,20 +60,28 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+# The code is just putting out a statement 4 < 9
+# The comparison is true
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+# The code is defining the variable books and assigning the integer 3 to it. Then it is asking if 4 is less than the variable.
+# The comparison is false because 4 is not less than 3
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+# The code is defining two variables, friends and siblings, each is assigned an integer. Then it is asking if friends is greater than siblings,
+# since friends equals 6 and siblings equals 2 the comparison is true, because 6 is greater than 2
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+# Thw code is assigning integers to the variables attendees and meals, then it is putting that attendees is not equal to meals, the comparison is true
+# since 9 does not equal 8.
 
 
 #-------------------
@@ -90,15 +104,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
+puts loves_to_play && loves_treats
 
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: It evaluated to 1 because the age is assigned and integer not a true or a false.
