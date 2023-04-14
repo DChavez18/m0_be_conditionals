8a624f645d8bf028239d1e02a386832d94e28140
Derek Chavez <129697286+DChavez18@users.noreply.github.com>
2023-04-13 23:19:57 -0600

    Update if_statements.rb

--- if_statements.rb
+++ if_statements.rb
@@ -66,12 +66,8 @@ end
 cups_of_flour = 5
 has_sauce = false
 
-if cups_of_flour == 1 && has_sauce == true
-  puts "I cannot make pizza"
-elsif cups_of_flour == 5 && has_sauce == false
-  puts "I cannot make pizza"
-elsif cups_of_flour == 2 && has_sauce == true
-  puts "I can mak pizza"
-elsif cups_of_flour == 3 && has_sauce == true
+if cups_of_flour >= 2 && has_sauce == true
   puts "I can make pizza"
+else
+  puts "I cannot make pizza"
 end
